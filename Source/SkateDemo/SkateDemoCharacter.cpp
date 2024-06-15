c535ad34bc9edb827b1253d310b8eb90df3a936a
Danielumana <daniel.umana.lopez@gmail.com>
2024-06-15 12:13:38 -0500

    Based implementation of player movement

--- Source/SkateDemo/SkateDemoCharacter.cpp
+++ Source/SkateDemo/SkateDemoCharacter.cpp
@@ -92,25 +92,23 @@ void ASkateDemoCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInput
 
 void ASkateDemoCharacter::Move(const FInputActionValue& Value)
 {
+	if (Controller == nullptr)
+	{
+		return;
+	}
 	// input is a Vector2D
 	FVector2D MovementVector = Value.Get<FVector2D>();
 
-	if (Controller != nullptr)
-	{
-		// find out which way is forward
-		const FRotator Rotation = Controller->GetControlRotation();
-		const FRotator YawRotation(0, Rotation.Yaw, 0);
+	// find out which way is forward
+	const FRotator Rotation = Controller->GetControlRotation();
+	const FRotator YawRotation(0, Rotation.Yaw, 0);
 
-		// get forward vector
-		const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
-	
-		// get right vector 
-		const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+	const FVector SkateForwardVector = SkateBoardSkeletalMesh->GetForwardVector();
+	const FVector SakateRightVector = SkateBoardSkeletalMesh->GetRightVector();
 
-		// add movement 
-		AddMovementInput(ForwardDirection, MovementVector.Y);
-		AddMovementInput(RightDirection, MovementVector.X);
-	}
+	// add movement 
+	AddMovementInput(SkateForwardVector, MovementVector.Y);
+	AddMovementInput(SakateRightVector, MovementVector.X);
 }
 
 void ASkateDemoCharacter::Look(const FInputActionValue& Value)
